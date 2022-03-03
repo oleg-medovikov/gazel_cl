294b590e47d1e1fb4da9ea4ebaca886fccb775c9
oleg-medovikov <oleg.medovikov@mail.ru>
2022-03-03 17:18:02 +0300

    config

--- config/config.py
+++ config/config.py
@@ -1,14 +1,14 @@
 from starlette.config import Config
 
+
+
 config = Config("config.cfg")
 
-DATABASE_URL=config("DATABASE_URL", cast=str)
-SALT=config("SALT", cast=str)
-DEFAULT_USERNAME=config("DEFAULT_USERNAME", cast=str)
 
-USER_FIRST_NAME = "test"
-USER_TOKEN = ""
-USER_POSITION = ""
+DATABASE_URL=config("DATABASE_URL", cast=str, default='')
 
+SALT=config("SALT", cast=str, default='')
 
-FONT_GRID_SIZE = config("FONT_GRID_SIZE", cast=str)
+DEFAULT_USERNAME=config("DEFAULT_USERNAME", cast=str, default='')
+
+FONT_GRID_SIZE = config("FONT_GRID_SIZE", cast=str, default='')
--- models/LoginUI.py
+++ models/LoginUI.py
@@ -1,16 +1,19 @@
 from kivy.uix.screenmanager import Screen
 
 from functions import login
-from config import DEFAULT_USERNAME
+from config import DEFAULT_USERNAME, DATABASE_URL
 from value import to_latin
 
 class LoginUI(Screen):
     "Класс первого окна входа"
     def on_enter(self):
-        if not DEFAULT_USERNAME == "": 
-            self.ids.username.text = DEFAULT_USERNAME
         self.ids.password.text = ""
-        self.ids.error.text = "Аутентификация"
+
+        if  DATABASE_URL != '': 
+            self.ids.username.text = DEFAULT_USERNAME
+            self.ids.error.text = "Аутентификация"
+        else:
+            self.ids.error.text = "Не найден config.cfg!"
 
     def insert_text(self, substring, from_undo=False):
         s = substring.translate(to_latin)
@@ -18,12 +21,14 @@ class LoginUI(Screen):
 
     def try_log_in(self):
         "попытка залогиниться при нажатии кнопки"
-        res = login(self.ids.username.text,self.ids.password.text)
-        if not res[0]:
-            "Выводим ошибку при провале"
-            self.ids.error.text = res[1] 
+        if DATABASE_URL != '':
+            res = login(self.ids.username.text,self.ids.password.text)
+            if not res[0]:
+                "Выводим ошибку при провале"
+                self.ids.error.text = res[1] 
+            else:
+                self.manager.transition.direction = "left"
+                self.manager.current = 'MainScreen'
         else:
-            self.manager.transition.direction = "left"
-            self.manager.current = 'MainScreen'
-
+            pass
 
